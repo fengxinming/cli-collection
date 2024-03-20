2e7700597c7ecd73e6269af065101afea866d5de
Jesse.Feng <fxm0016@126.com>
2024-03-20 21:17:35 +0800

    feat(bin): 支持读取config配置

--- src/bin.ts
+++ src/bin.ts
@@ -3,7 +3,8 @@
 import cac from 'cac';
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import { dirname, join, isAbsolute, extname } from 'node:path';
+import { createRequire } from 'node:module';
 import concat from './index';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,13 +16,44 @@ const cli = cac(pkg.name);
 cli
   .command('[...files]', pkg.description)
   .option('-o, --output <file>', 'output file')
-  .action((files, options) => {
+  .option('-c, --config <file>', 'specify a config file')
+  .action(async (files, { output, config } = {}) => {
+    let promise: Promise<unknown> = Promise.resolve('');
+    if (config) {
+      if (!isAbsolute(config)) {
+        config = join(process.cwd(), config);
+      }
+
+      let conf;
+      switch (extname(config)) {
+        case '.js':
+          conf = createRequire(import.meta.url)(config);
+          break;
+        case '.mjs':
+          conf = (await import(config)).default;
+          break;
+        case '.json':
+          conf = JSON.parse(readFileSync(config, 'utf-8'));
+          break;
+      }
+
+      if (conf) {
+        promise = concat(conf.input, conf.output, conf.options);
+      }
+    }
+    else if (output) {
+      promise = concat(files, output);
+    }
+
     const start = Date.now();
-    concat(files, options.output)
-    // eslint-disable-next-line no-console
-      .then((dest) => console.log(`\n created ${dest} in ${Date.now() - start} ms\n`))
-    // eslint-disable-next-line no-console
-      .catch((err) => console.error(err));
+    try {
+      const dest = await promise;
+      console.info(`\n created ${dest} in ${Date.now() - start} ms\n`);
+    }
+    catch (err) {
+      console.error(err);
+      process.exit(-1);
+    }
   });
 
 cli.version(pkg.version);
