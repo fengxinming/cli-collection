1dfaf5d74ca26c631a699ba013e3961dc4baa020
Jesse.Feng <fxm0016@126.com>
2025-05-01 18:04:35 +0800

    refactor(delete files): 删除历史文件

--- test/files/isArrayLike.js
+++
@@ -1,6 +0,0 @@
-import isFunction from 'celia.is/isFunction';
-import isNumber from 'celia.is/isNumber';
-
-export default function (value) {
-  return !!value && isNumber(value.length) && !isFunction(value);
-};
